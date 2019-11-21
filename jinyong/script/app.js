2afb9b62dad69d4af6ab9a37677008a73b86d600
doublejbs <doublejbs@naver.com>
2019-11-21 15:55:29 +0900

    init

---
+++ jinyong/script/app.js
@@ -0,0 +1,24 @@
+var video = document.querySelector('video');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var localMediaStream = null;
+var errorCallback = function (e) {
+    console.log('Reeeejected!', e);
+};
+function snapshot() {
+    video.play();
+    if (localMediaStream) {
+        ctx.drawImage(video, 0, 0);
+        ctx.translate(canvas.width, 0);
+        ctx.scale(-1, 1);
+        // "image/webp" works in Chrome.
+        // Other browsers will fall back to image/png.
+        //document.querySelector('img').src = canvas.toDataURL('image/webp');
+    }
+}
+
+// Not showing vendor prefixes or code that works cross-browser.
+navigator.getUserMedia({ video: true }, function (stream) {
+    video.srcObject = stream;
+    localMediaStream = stream;
+}, errorCallback);
