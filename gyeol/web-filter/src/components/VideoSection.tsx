a52888569ff3a4e67c773a987e6bdb9001b93970
gyeol1212 <gyeol1212@likelion.org>
2019-11-21 16:31:47 +0900

    video section 세팅

--- gyeol/web-filter/src/components/VideoSection.tsx
+++ gyeol/web-filter/src/components/VideoSection.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 
-const VideoSection: React.FC = (): JSX.Element => {
-  return <Container>videoSection</Container>;
+interface IProps {
+  setVideo: React.Dispatch<React.SetStateAction<HTMLVideoElement | null>>;
+  size: { width: number; height: number };
+}
+
+const VideoSection: React.FC<IProps> = ({ setVideo, size }): JSX.Element => {
+  const videoEl = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (!videoEl) {
+      return;
+    }
+    setVideo(videoEl.current);
+  }, []);
+
+  return (
+    <Container>
+      <Video ref={videoEl} size={size} />
+    </Container>
+  );
 };
 
 export default VideoSection;
 
-const Container = styled.div`
-  width: 600px;
-  height: 450px;
-  border: 1px solid black;
+const Container = styled.div``;
+
+interface IVideo {
+  size: { width: number; height: number };
+}
+const Video = styled.video`
+  width: ${(props: IVideo) => `${props.size.width}px`};
+  height: ${(props: IVideo) => `${props.size.height}px`};
+  border: 2px solid lightgray;
 `;
--- gyeol/web-filter/src/containers/WebFilter.tsx
+++ gyeol/web-filter/src/containers/WebFilter.tsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import VideoSection from "../components/VideoSection";
 import FilterSection from "../components/FilterSection";
 import Gallary from "../components/Gallary";
 
 const WebFilter: React.FC = (): JSX.Element => {
+  const [video, setVideo] = useState<HTMLVideoElement | null>(null);
+  const [size, setSize] = useState<{ width: number; height: number }>({
+    width: 400,
+    height: 250
+  });
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: true
+      })
+      .then((stream: MediaStream) => {
+        if (video) {
+          video.srcObject = stream;
+          video.onloadedmetadata = (): void => {
+            const height = video.videoHeight / (video.videoWidth / size.width);
+            setSize({ ...size, height });
+          };
+
+          // 준비 되면 play
+          video.oncanplaythrough = (): void => {
+            video.play();
+          };
+        }
+      })
+      .catch(err => {
+        console.log(`ERROR : ${err}`);
+      });
+  }, [video]);
   return (
     <Container>
-      <VideoSection />
+      <VideoSection setVideo={setVideo} size={size} />
       <FilterSection />
       <Gallary />
     </Container>
