d8ff088632b52f0f67d301d19924cf97a0cd9b61
doublejbs <doublejbs@naver.com>
2019-11-21 22:49:05 +0900

    별 하트 추가
    별, 하트 아이템 필터 추가

--- jinyong/script/app.js
+++ jinyong/script/app.js
@@ -1,11 +1,18 @@
 let faceapi;
 let video;
 let detections;
+let star, heart;
+let itemName = '';
 
 const detectionOptions = {
     withLandmarks: true,
     withDescriptors: false
 };
+
+function preload() {
+    star = loadImage('assets/star.png');
+    heart = loadImage('assets/heart.png');
+}
 
 function setup() {
     const x = (windowWidth - width) / 2;
@@ -26,22 +33,26 @@ function createButtons(x, y){
     button1 = createButton('item1');
 
     button1.position(x, y + height);
-    button1.mousePressed(changeBG);
-
+    button1.mousePressed(function(){
+        itemName = 'star';
+    });
+    
     button2 = createButton('item2');
 
     button2.position(x + width / 4, y + height);
-    button2.mousePressed(changeBG);
+    button2.mousePressed(function(){
+        itemName = 'heart';
+    });
 
     button3 = createButton('item3');
 
     button3.position(x + width * 2 / 4, y + height);
-    button3.mousePressed(changeBG);
+    button3.mousePressed();
 
     button4 = createButton('item4');
 
     button4.position(x + width * 3 / 4, y + height);
-    button4.mousePressed(changeBG);
+    button4.mousePressed();
 }
 
 function initVideo(){
@@ -63,34 +74,17 @@ function gotResults(err, result) {
         console.log(err);
         return;
     }
-
     detections = result;
+
     background(255);
     image(video, 0, 0, width, height);
 
     if (detections && detections.length > 0) {
-        drawBox(detections);
         drawLandmarks(detections);
     }
-
     requestAnimationFrame(function () {
         faceapi.detect(gotResults)
     });
-}
-
-function drawBox(detections) {
-    for (let i = 0; i < detections.length; i++) {
-        const alignedRect = detections[i].alignedRect;
-        const x = alignedRect._box._x;
-        const y = alignedRect._box._y;
-        const boxWidth = alignedRect._box._width;
-        const boxHeight = alignedRect._box._height;
-
-        noFill();
-        stroke(161, 95, 251);
-        strokeWeight(2);
-        rect(x, y, boxWidth, boxHeight);
-    }
 }
 
 function drawLandmarks(detections) {
@@ -101,43 +95,40 @@ function drawLandmarks(detections) {
     for (let i = 0; i < detections.length; i++) {
         const { mouth, nose, leftEye, rightEye, rightEyeBrow, leftEyeBrow } = detections[i].parts;
 
-        drawPart(mouth, true);
-        drawPart(nose, false);
-        drawPart(leftEye, true);
-        drawPart(leftEyeBrow, false);
-        drawPart(rightEye, true);
-        drawPart(rightEyeBrow, false);
+        if (itemName === 'star'){
+            drawPart(leftEye, true);
+            drawPart(rightEye, true);
+        }
+        else if (itemName === 'heart'){
+            drawPart(leftEye, true);
+            drawPart(rightEye, true);
+        }
+        else if (itemName === ''){
+            continue;
+        }
     }
 }
 
 function drawPart(features, closed) {
     beginShape();
 
-    for (const feature of features) {
-        const { _x: x, _y: y } = feature;
+    let totalX = 0;
+    let totalY = 0;
 
-        vertex(x, y);
+    for (const feature of features) {
+        totalX += feature._x;
+        totalY += feature._y;
     }
     
-    if (closed === true) {
-        endShape(CLOSE);
-    } else {
-        endShape();
+    totalX /= features.length;
+    totalY /= features.length;
+
+    if (itemName == 'star'){
+        image(star, totalX - 15, totalY - 15, 30, 30);
     }
-}
-
-function drawImageList(imageList){
-    let xIdx = 0;
-
-    for (let i = 0; i < imageList.length; i++){
-        Image(imageList[i], xIdx, height);
-
-        xIdx += imageList[i].width;
+    else if (itemName == 'heart'){
+        image(heart, totalX - 15, totalY - 15 + 30, 30, 30);
     }
-}
-
-function changeBG() {
-    const val = random(255);
-
-    background(val);
+    
+    endShape(closed ? CLOSE : undefined);
 }
