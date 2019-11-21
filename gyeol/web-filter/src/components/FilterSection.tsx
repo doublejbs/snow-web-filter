7c276174f2587d5a120be68d86f53046d6065e03
gyeol1212 <gyeol1212@likelion.org>
2019-11-21 17:25:42 +0900

    Filter Button 세팅

--- gyeol/web-filter/src/components/FilterSection.tsx
+++ gyeol/web-filter/src/components/FilterSection.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
+import Button from "./atoms/Button";
+
 const FilterSection: React.FC = (): JSX.Element => {
-  return <Container>FILTER</Container>;
+  return (
+    <Container>
+      <Button>필터 1</Button>
+      <Button>필터 2</Button>
+      <Button>필터 3</Button>
+      <CaptureButton>캡쳐</CaptureButton>
+    </Container>
+  );
 };
 
 export default FilterSection;
@@ -11,4 +20,9 @@ const Container = styled.div`
   border: 1px solid black;
   margin: 1rem;
   padding: 1rem;
+  display: flex;
+`;
+
+const CaptureButton = styled(Button)`
+  border-radius: 0;
 `;
