5aaf7f0a0601d00bd1c49047b1b07362453f69b2
doublejbs <doublejbs@naver.com>
2019-11-22 10:32:50 +0900

    갤러리 리스트 추가
    갤러리 리스트 추가

--- jinyong/script/app.js
+++ jinyong/script/app.js
@@ -20,8 +20,8 @@ function preload() {
 }
 
 function setup() {
-    const x = (windowWidth) / 2 - width;
-    const y = (windowHeight) / 2 - height;
+    const x = (windowWidth) / 5;
+    const y = (windowHeight) / 4;
 
     frameRate(120);
     initCanvas(x, y);
@@ -58,17 +58,22 @@ function createButtons(x, y){
     });
 
     button4 = createButton('촬영');
+    const listDiv = createElement('div');
 
+    listDiv.size(width, height);
+    listDiv.position(x + width + 10, 10);
     button4.position(x + width * 3 / 4, y + height);
     button4.mousePressed(function(){
         const data = cnv.canvas.toDataURL();
         const a = createElement('a');
         const img = createElement('img');
-
+        
+        img.size(width / 2, height / 2);
         img.elt.src = data;
         a.elt.href = data;
         a.elt.download = 'image' + str(downloadName++) + '.jpg';
         a.child(img);
+        listDiv.child(a);
     });
 }
 
